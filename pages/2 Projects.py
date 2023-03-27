fa571722bccb30c2f5bf1ba3f8aa46546f6d6911
Ryan McIntosh <ryan895@revature.net>
2023-03-27 00:01:35 -0500

    fixed projects page to automatically update when repos are changed

--- pages/2 Projects.py
+++ pages/2 Projects.py
@@ -1,17 +1,35 @@
 import streamlit as st
-
+import requests
+from github import Github
+from copy import deepcopy
 
 
 owner = "Ryintosh"
 branch = "main"
-repoName = ["portfolio-website", "temp", "temp"]
+repoName = []
+
+g = Github()
+user = g.get_user(owner)
+
+for repo in user.get_repos():
+    repoName.append(repo.name)
 
 
-portfoliowebsite, tab2, tab3 = st.tabs(tabs = repoName)
-
-with portfoliowebsite:
-    with open("ReadMes/portfolio-website.md") as f:
-        s = f.read()
-    st.markdown(s)
+repoName.append("test")
+temp = deepcopy(repoName)
 
 
+def getReadMe(repo,owner,branch):
+    test = requests.get(url = f"https://raw.githubusercontent.com/{owner}/{repo}/{branch}/README.md")
+    return test
+
+
+repoName = st.tabs(repoName)
+
+i=0
+while i <= len(repoName) - 1:
+    with repoName[i]:
+        response = getReadMe(temp[i],owner,branch)
+        st.write(temp[i])
+        st.write(response.text)
+    i = i + 1
